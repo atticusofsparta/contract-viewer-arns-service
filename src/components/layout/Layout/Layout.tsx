f2abb452549b1821cb540a26f97b1013eba0c954
atticusofsparta <atticusofsparta@protonmail.com>
2023-07-03 01:51:20 -0600

    switch to yarn

--- src/components/layout/Layout/Layout.tsx
+++ src/components/layout/Layout/Layout.tsx
@@ -1,4 +1,3 @@
-import Matrix from 'react-matrix-effect';
 import { Outlet } from 'react-router-dom';
 
 import { Navbar } from '..';
@@ -8,17 +7,6 @@ function Layout() {
   return (
     <div>
       <Navbar />
-      <Matrix
-        fullscreen={true}
-        style={{
-          zIndex: '1000',
-          position: 'fixed',
-          background: 'none',
-          display: 'flex',
-          opacity: '0.3',
-        }}
-        speed={0.5}
-      />
 
       <Outlet />
       <Notifications />
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App.tsx';
+import App from './App';
 import './index.css';
-import { reducer } from './state/GlobalReducer.tsx';
-import GlobalStateProvider from './state/GlobalState.tsx';
+import { reducer } from './state/GlobalReducer';
+import GlobalStateProvider from './state/GlobalState';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
